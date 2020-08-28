d0bcdc02c0014d605c3e2a2673adbcbf4c756d7d
anh.phan <anh.phan@savvycomsoftware.com>
2020-08-28 08:58:14 +0700

    cập nhật ngày 28-08-2020

--- NumberAndCurrencyConversion.ts
+++ NumberAndCurrencyConversion.ts
@@ -13,14 +13,14 @@ console.log(accounting.formatMoney(123456789));
 console.log(accounting.formatMoney(123456789, '$ ', 2));
 // $ 123,456,789.00 ('$ ' + '123,456,789' + '.00')
 
-console.log(accounting.formatMoney(123456789, '$ ', 3, '.', ','));
-// $ 123.456.789,00 ('$ ' + '123(.)456(.)789' + '(,)000')
+console.log(accounting.formatMoney(123456789, '$ ', 2, '.', ','));
+// $ 123.456.789,00 ('$ ' + '123(.)456(.)789' + '(,)00')
 
 console.log(accounting.formatMoney(123456789, { symbol: 'USD', format: '%v %s' }));
 // 123,456,789.00 USD ('123,456,789.00' (%v = value) + ' ' + 'USD' (%s = symbol))
 
 console.log(accounting.formatNumber(123456789, 2, ',', '.'));
-// 123,456,789.00
+// '123(,)456(,)789' + '().)00'
 
 console.log(accounting.unformat('£ 1,234,567.89 - GBP'));
 // 1234567.89 (number)
@@ -28,5 +28,5 @@ console.log(accounting.unformat('£ 1,234,567.89 - GBP'));
 console.log(numeral(123456789).format('0,0.00 $'));
 // 123,456,789.00 $
 
-console.log(numeral(123456789).format('($ 0.00 a)'));
+console.log(numeral(1234).format('($ 0.00a)'));
 // $ 123.46 m
