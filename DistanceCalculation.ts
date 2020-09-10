e48128ef13cb242321510ed8e6f7293f3956a9f7
Phan T. Anh <54684536+anhphantien@users.noreply.github.com>
2020-09-10 10:31:02 +0700

    Update DistanceCalculation.ts

--- DistanceCalculation.ts
+++ DistanceCalculation.ts
@@ -1,12 +1,12 @@
 import { getPreciseDistance } from 'geolib';
 
 export const getDistance = (
-  startingPoint: { lat1: string | number, lng1: string | number },
+  startPoint: { lat1: string | number, lng1: string | number },
   endPoint: { lat2: string | number, lng2: string | number },
 ) => {
   return getPreciseDistance({
-    latitude: startingPoint.lat1,
-    longitude: startingPoint.lng1,
+    latitude: startPoint.lat1,
+    longitude: startPoint.lng1,
   }, {
     latitude: endPoint.lat2,
     longitude: endPoint.lng2,
