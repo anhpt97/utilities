a8bb9ebac7ae26322e63e7496a08360d02ca3ca5
anh.phan <anh.phan@savvycomsoftware.com>
2020-10-29 17:38:16 +0700

    có phải anh đang trêu đùa em đấy không?

--- ExtractZippedFileAndCompressFile(s).ts
+++ ExtractZippedFileAndCompressFile(s).ts
@@ -12,15 +12,27 @@ import archiver = require('archiver');
 export const compress = () => {
   const archive = archiver('zip', { zlib: { level: 9 } }); // level must be >= -1 and <= 9
   archive.append(Buffer.from('Hello World!'), { name: 'HelloWorld.txt' });
+  // archive.file('/home/anhphan/Downloads/sample.jpg', { name: 'sample.jpg' }); // append local file
   archive.finalize();
-  archive.pipe(fs.createWriteStream('./sample.rar'));
+  archive.pipe(fs.createWriteStream('./sample.zip'));
   // archive.pipe(res.attachment(`${Date.now()}.zip`));
 }
 
 import AdmZip = require('adm-zip');
 
 export const extract2 = (filePath: string) => {
-  fs.writeFileSync('./sample.zip', fs.readFileSync(filePath)); // ghi file ra một chỗ khác
-  new AdmZip('./sample.zip').extractAllTo(`${process.cwd()}/sample`);
+  fs.writeFileSync('./sample.zip', fs.readFileSync(filePath)); // ghi file nén ra một chỗ khác
+  const zip = new AdmZip('./sample.zip');
+  // const zipEntries = zip.getEntries();
+  // zipEntries.map(zipEntry => zipEntry.entryName); // lấy danh sách tên các file được nén
+  zip.extractAllTo(`${process.cwd()}/sample`);
   fs.unlinkSync('./sample.zip');
+}
+
+export const compress2 = () => {
+  const zip = new AdmZip();
+  zip.addFile('HelloWorld.txt', Buffer.from('Hello World!'));
+  // zip.addLocalFile('/home/anhphan/Downloads/sample.jpg'); // append local file
+  zip.writeZip('./sample.zip');
+  // res.send(zip.toBuffer());
 }
