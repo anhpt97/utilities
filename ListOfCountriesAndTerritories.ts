a51bf5b3657d4055cfb5e0950af721446a9dc95e
anh.phan <anh.phan@savvycomsoftware.com>
2020-12-02 14:18:09 +0700

    ối dồi ôi

--- ListOfCountriesAndTerritories.ts
+++ ListOfCountriesAndTerritories.ts
@@ -1,17 +1,5 @@
 import countries = require('i18n-iso-countries');
 
 export const getListOfCountriesAndTerritories = () => {
-  countries.getNames('en').RU = 'Russia Federation';
-  countries.getNames('en').GB = 'United Kingdom';
-  countries.getNames('en').US = 'United States of America';
   return countries.getNames('en');
 };
-
-// export const getListOfCountriesAndTerritories = () => {
-//   return Object.keys(countries.getNames('en')).reduce((prev, cur) => {
-//     if (Array.isArray(countries.getNames('en')[cur])) {
-//       return { ...prev, [cur]: countries.getNames('en')[cur][0] };
-//     }
-//     return { ...prev, [cur]: countries.getNames('en')[cur] };
-//   }, {});
-// }
